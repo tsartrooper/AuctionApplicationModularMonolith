9949fe5f778ac91a95927e52771d9f2f71e78c9c
Prathamesh Mane <116452100+tsartrooper@users.noreply.github.com>
2025-04-05 14:09:53 +0530

    Update AuctionApplicationTests.java

--- auction_application/src/test/java/com/example/auction_application/AuctionApplicationTests.java
+++ auction_application/src/test/java/com/example/auction_application/AuctionApplicationTests.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
