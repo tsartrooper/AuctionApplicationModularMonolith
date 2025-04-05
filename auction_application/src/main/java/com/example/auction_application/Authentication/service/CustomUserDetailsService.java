0205f027f8e7af240abb9e931d8b98bbfcd43938
Prathamesh Mane <116452100+tsartrooper@users.noreply.github.com>
2025-04-05 14:02:59 +0530

    Update CustomUserDetailsService.java

--- auction_application/src/main/java/com/example/auction_application/Authentication/service/CustomUserDetailsService.java
+++ auction_application/src/main/java/com/example/auction_application/Authentication/service/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package com.example.auction_application.Authentication.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
