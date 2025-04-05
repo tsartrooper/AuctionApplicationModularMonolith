33028deb98fbc8792402b798ecb7373fd82031fb
Prathamesh Mane <116452100+tsartrooper@users.noreply.github.com>
2025-04-05 14:04:10 +0530

    Update AuctionBiddingPort.java

--- auction_application/src/main/java/com/example/auction_application/Bid/port/AuctionBiddingPort.java
+++ auction_application/src/main/java/com/example/auction_application/Bid/port/AuctionBiddingPort.java
@@ -2,6 +2,7 @@ package com.example.auction_application.Bid.port;
 
 public interface AuctionBiddingPort {
     void validateBid(long auctionId, long bidderId, double amount) throws Exception;
+    
     long getCurrentHighestBidderId(long auctionId) throws Exception;
     boolean isAuctionActive(long auctionId) throws Exception;
     void updateBid(long auctionId, long bidderId, double amount, long bidId) throws Exception;
