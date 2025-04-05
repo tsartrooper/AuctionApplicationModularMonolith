90ca0d620e443e6c8c9aa89a04c7b5d24e5788b9
Prathamesh Mane <116452100+tsartrooper@users.noreply.github.com>
2025-04-05 14:01:00 +0530

    Update AuthenticationRequestDTO.java

--- auction_application/src/main/java/com/example/auction_application/Authentication/dto/AuthenticationRequestDTO.java
+++ auction_application/src/main/java/com/example/auction_application/Authentication/dto/AuthenticationRequestDTO.java
@@ -2,6 +2,7 @@ package com.example.auction_application.Authentication.dto;
 
 import java.io.Serializable;
 
+
 public class AuthenticationRequestDTO  implements Serializable{
     private static final long serialVersionUID = 1L;
     String userName;
