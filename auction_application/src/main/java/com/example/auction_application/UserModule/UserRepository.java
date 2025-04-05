ed88fab8aeed3a9c3f77e08d7df06f2964b6b536
Prathamesh Mane <116452100+tsartrooper@users.noreply.github.com>
2025-04-05 13:52:44 +0530

    Update UserRepository.java

--- auction_application/src/main/java/com/example/auction_application/UserModule/UserRepository.java
+++ auction_application/src/main/java/com/example/auction_application/UserModule/UserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.auction_application.UserModule.entity.WebUser;
 
 
+
 public interface UserRepository extends JpaRepository<WebUser, Long>{
     public Optional<WebUser> findByUserName(String userName);
 }
