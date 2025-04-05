0402dd7208bf6fbf25163c32b34ece0273f121ba
Prathamesh Mane <116452100+tsartrooper@users.noreply.github.com>
2025-04-05 14:00:38 +0530

    Update SpringSecurityConfiguration.java

--- auction_application/src/main/java/com/example/auction_application/Authentication/Config/SpringSecurityConfiguration.java
+++ auction_application/src/main/java/com/example/auction_application/Authentication/Config/SpringSecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.auction_application.Authentication.Config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
