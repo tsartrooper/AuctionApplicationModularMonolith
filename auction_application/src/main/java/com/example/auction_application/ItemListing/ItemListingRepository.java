264d6c9a93b04fe9f7b6ad611591d89e67da2e75
Prathamesh Mane <116452100+tsartrooper@users.noreply.github.com>
2025-04-05 14:07:46 +0530

    Update ItemListingRepository.java

--- auction_application/src/main/java/com/example/auction_application/ItemListing/ItemListingRepository.java
+++ auction_application/src/main/java/com/example/auction_application/ItemListing/ItemListingRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.auction_application.ItemListing.entity.ItemListing;
 
 
+
+
 public interface ItemListingRepository extends JpaRepository<ItemListing, Long> {
     
     public List<ItemListing> findByOwnerId(Long id);
