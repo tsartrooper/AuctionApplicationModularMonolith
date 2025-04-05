85793ce8911f4ddd8b8aa848d104d5aca91aebba
Prathamesh Mane <116452100+tsartrooper@users.noreply.github.com>
2025-04-05 13:54:41 +0530

    Update WebSocketConfig.java

--- auction_application/src/main/java/com/example/auction_application/WebSocketConfig.java
+++ auction_application/src/main/java/com/example/auction_application/WebSocketConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 
+
 import com.example.auction_application.AuctionListing.AuctionWebSocketHandler;
 
 
