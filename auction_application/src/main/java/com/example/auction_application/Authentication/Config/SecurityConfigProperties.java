f754ac630c760339edcdddb05c5c5b8e5c1a6a25
Prathamesh Mane <116452100+tsartrooper@users.noreply.github.com>
2025-04-05 14:00:32 +0530

    Update SecurityConfigProperties.java

--- auction_application/src/main/java/com/example/auction_application/Authentication/Config/SecurityConfigProperties.java
+++ auction_application/src/main/java/com/example/auction_application/Authentication/Config/SecurityConfigProperties.java
@@ -2,6 +2,7 @@ package com.example.auction_application.Authentication.Config;
 
 import java.util.List;
 
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
