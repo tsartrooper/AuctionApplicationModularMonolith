3e5f93adce3c1dd01dc43c391db130b973c082d1
Prathamesh Mane <116452100+tsartrooper@users.noreply.github.com>
2025-04-05 13:58:24 +0530

    Update AuctionCatalogController.java

--- auction_application/src/main/java/com/example/auction_application/AuctionListing/api/AuctionCatalogController.java
+++ auction_application/src/main/java/com/example/auction_application/AuctionListing/api/AuctionCatalogController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
