3975d4bfc6c4be0195ba1013b4a5df777998f92f
Prathamesh Mane <116452100+tsartrooper@users.noreply.github.com>
2025-04-05 13:49:16 +0530

    Update BidRequestDTO.java

--- auction_application/src/main/java/com/example/auction_application/Bid/dto/BidRequestDTO.java
+++ auction_application/src/main/java/com/example/auction_application/Bid/dto/BidRequestDTO.java
@@ -3,6 +3,7 @@ package com.example.auction_application.Bid.dto;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+
 public class BidRequestDTO implements Serializable{
     private static final long serialVersionUID = 1L;
     private double amount;
